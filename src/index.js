7e8919f917faf3e0d1999c68a57793d444c997dc
Daniya Yelemessova <daniya@collingsmail.com>
2024-01-02 14:51:31 -0500

    Add changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+
+function triviaGame() {
+  let request = new XMLHttpRequest();
+  const url = `https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=boolean`;
+
+  request.addEventListener("loadend", function () {
+    const response = JSON.parse(this.responseText);
+    
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
+
+
